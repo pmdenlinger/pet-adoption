1220e3c5b992afb575b34f938a333f5201ad2654
Paul Denlinger <pmdenlinger@icloud.com>
2024-03-17 10:42:51 -0700

    Change pet names into dynamic object

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const template = document.querySelector("#pet-card-template");
+const wrapper = document.createDocumentFragment();
+
 async function start() {
   const weatherPromise = await fetch(
     "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
@@ -18,8 +21,13 @@ async function petsArea() {
   );
   const petsData = await petsPromise.json();
   petsData.forEach((pet) => {
-    console.log(pet.species);
+    const clone = template.content.cloneNode(true);
+
+    clone.querySelector("h3").textContent = pet.name;
+
+    wrapper.appendChild(clone);
   });
+  document.querySelector(".list-of-pets").appendChild(wrapper);
 }
 
 petsArea();
